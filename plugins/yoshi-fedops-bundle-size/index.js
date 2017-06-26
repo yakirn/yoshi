65e77473dee39191a11bc44881686d71dbc89403
Jonathan Adler <jonathana@wix.com>
2017-06-26 15:30:41 +0300

    add bundle size plugin

--- plugins/yoshi-fedops-bundle-size/index.js
+++ plugins/yoshi-fedops-bundle-size/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const path = require('path');
-const grafanaApi = require('fedops-grafana-api');
+const shelljs = require('shelljs');
+const fs = require('fs');
 
-// move to a 3rd party?
 const tryRequire = name => {
   try {
     return require(name);
@@ -12,8 +12,55 @@ const tryRequire = name => {
   }
 };
 
-module.exports = ({inTeamCity}) => {
-  function fedopsSync() {
+const isSingleEntry = entry => typeof entry === 'string' || Array.isArray(entry);
+
+const getBundleNames = config => {
+  const {defaultEntry, entry: customEntry} = config;
+  const entryGetter = customEntry || defaultEntry || (() => null);
+  const entry = entryGetter();
+  return isSingleEntry(entry) ? 'app' : Object.keys(entry);
+}
+
+const command = ({appName, bundleName, bundleSize, timestamp}) => {
+  const args = {
+    'wix-bi-tube.root': 'events_catalog',
+    src: '72',
+    'app_name': appName,
+    'bundle_name': bundleName,
+    'bundle_size': bundleSize
+  };
+
+  return ''.concat(
+    'echo `',
+    Object.keys(args).map(key => `${key}=${args[key]}`).join('.'),
+    ' ',
+    timestamp,
+    '` | nc -q0 m.wixpress.com 2003'
+  );
+}
+
+const shellExec = (config, timestamp) => bundleName => {
+  return new Promise(resolve => {
+    fs.stat(path.resolve(process.cwd(), `dist/${bundleName}.bundle.min.js`), (err, stats) => {
+      if (!err) {
+        const bundleSize = stats.size;
+        const params = {
+          appName: config.app_name,
+          bundleName,
+          bundleSize,
+          timestamp
+        };
+        shelljs.exec(command(params), resolve);
+        return;
+      }
+      console.warn(`Error code ${err.code}. Failed to find size of file ${bundleName}.bundle.min.js.`);
+      resolve();
+    });
+  });
+}
+
+module.exports = ({log, inTeamCity, projectConfig}) => {
+  function fedopsBundleSize() {
     if (!inTeamCity()) {
       return Promise.resolve();
     }
@@ -24,10 +71,19 @@ module.exports = ({inTeamCity}) => {
       return Promise.resolve();
     }
 
-    return grafanaApi.sync(config)
-      .then(() => console.log('Fedops: sync succeeded'))
-      .catch(e => console.log('grafana sync failure: status', e.status, 'message:', e.response && e.response.text));
+    let promises = [];
+
+    try {
+      let fileName = getBundleNames(projectConfig);
+      fileName = Array.isArray(fileName) ? fileName : [fileName];
+      promises = fileName.map(shellExec(config, Date.now()));
+    } catch (e) {
+      console.warn('Bundle size report error:', e);
+      return Promise.resolve();
+    }
+
+    return log(Promise.all(promises));
   }
 
-  return fedopsSync;
+  return fedopsBundleSize;
 };
--- plugins/yoshi-fedops-bundle-size/test/index.spec.js
+++ plugins/yoshi-fedops-bundle-size/test/index.spec.js
@@ -1,46 +1,151 @@
 'use strict';
-
-// const {expect} = require('chai');
-const stripAnsi = require('strip-ansi');
+const sinonChai = require('sinon-chai');
+const chai = require('chai');
 const intercept = require('intercept-stdout');
 const tp = require('test-phases');
-const mockBin = require('mock-bin');
-const fedopsSync = require('../index');
+const proxyquire = require('proxyquire');
+const sinon = require('sinon');
+chai.use(sinonChai);
+const {expect} = chai;
+
+const shellExecSpy = sinon.spy((str, cb) => {
+  cb(str);
+});
+const fedopsBundleSize = proxyquire('../index', {
+  'shelljs': {exec: shellExecSpy}
+});
+
+const APP_NAME = 'your-unique-app-name'; // eslint-disable-line camelcase
+const fedopsJson = JSON.stringify({
+  app_name: APP_NAME
+}, null, 2);
 
 describe('measure bundle size', () => {
+  const timestamp = (new Date('2017-06-26')).getTime();
+  const someFileContent = `console.log('hello world');`;
+  const someOtherFileContent = `console.log('foo bar');`;
+  const bundleSize = content => content.length + 1;
+  let clock;
   let test;
-  let cleanup;
-  let stdout;
 
-  const createTask = options => {
+  const createTask = (options = {}) => {
+    const {projectConfig} = options;
+    const rest = Object.assign({}, options);
+    delete rest.projectConfig;
+
     const defaults = {
+      log: a => a,
       inTeamCity: () => true,
-      projectConfig: {defaultEntry: () => 'src/index.js'},
+      projectConfig: Object.assign({defaultEntry: () => 'src/index.js'}, projectConfig),
     };
 
-    return fedopsSync(Object.assign({}, defaults, options));
+    return fedopsBundleSize(Object.assign({}, defaults, rest));
   };
 
-  before(() => cleanup = intercept(s => {
-    stdout += stripAnsi(s);
-  }));
-  beforeEach(() => test = tp.create());
-  beforeEach(() => process.chdir(test.tmp));
+  const output = (bundleName, fileContent) => {
+    return `echo \`wix-bi-tube.root=events_catalog.src=72.app_name=${APP_NAME}.bundle_name=${bundleName}.bundle_size=${bundleSize(fileContent)} ${timestamp}\` | nc -q0 m.wixpress.com 2003`;
+  }
 
-  afterEach(() => test.teardown());
-  afterEach(() => stdout = '');
-  after(() => cleanup());
+  beforeEach(() => {
+    test = tp.create();
+    process.chdir(test.tmp);
+    clock = sinon.useFakeTimers(timestamp);
+  });
 
-  it('should report the size off app.bundle.min given no entry point', async () => {
+  afterEach(() => {
+    test.teardown();
+    shellExecSpy.reset();
+    clock.restore();
+  });
+
+  it('should\'nt do anything if not in team city', () => {
     test.setup({
-      'dist/app.bundle.min.js': `
-        console.log('hello world);
-      `
+      'dist/app.bundle.min.js': someFileContent,
+      'fedops.json': fedopsJson
     });
-    const unmock = await mockBin('echo', 'node', 'console.log(arguments)');
-    const task = createTask();
-    task().then(() => {
+    const task = createTask({inTeamCity: () => false});
+    return task().then(() => {
+      expect(shellExecSpy).to.have.been.notCalled;
+    });
+  });
 
+  it('should\'nt do anything if no fedops config found', () => {
+    test.setup({
+      'dist/app.bundle.min.js': someFileContent
+    });
+    const task = createTask({inTeamCity: () => false});
+    return task().then(() => {
+      expect(shellExecSpy).to.have.been.notCalled;
+    });
+  });
+
+  it('should report the size off app.bundle.min given no entry point', () => {
+    test.setup({
+      'dist/app.bundle.min.js': someFileContent,
+      'fedops.json': fedopsJson
+    });
+    const task = createTask();
+    return task().then(() => {
+      expect(shellExecSpy).to.have.been.calledOnce;
+      expect(shellExecSpy).to.have.been.calledWith(output('app', someFileContent));
+    });
+  });
+
+  it('should report the size off app.bundle.min given a string entry point', () => {
+    test.setup({
+      'dist/app.bundle.min.js': someFileContent,
+      'fedops.json': fedopsJson
+    });
+    const task = createTask({projectConfig: {entry: () => 'src/index2.js'}});
+    return task().then(() => {
+      expect(shellExecSpy).to.have.been.calledOnce;
+      expect(shellExecSpy).to.have.been.calledWith(output('app', someFileContent));
+    });
+  });
+
+  it('should report the size off app.bundle.min given a string entry point', () => {
+    test.setup({
+      'dist/app.bundle.min.js': someFileContent,
+      'fedops.json': fedopsJson
+    });
+    const task = createTask({projectConfig: {entry: () => ['src/index2.js', 'src/index3.js']}});
+    return task().then(() => {
+      expect(shellExecSpy).to.have.been.calledOnce;
+      expect(shellExecSpy).to.have.been.calledWith(output('app', someFileContent));
+    });
+  });
+
+  it('should report the size of all keys in entry object', () => {
+    const entry = {
+      a: 'src/a.js',
+      b: 'src/b.js'
+    };
+    test.setup({
+      'dist/a.bundle.min.js': someFileContent,
+      'dist/b.bundle.min.js': someOtherFileContent,
+      'fedops.json': fedopsJson
+    });
+    const task = createTask({projectConfig: {entry: () => entry}});
+    return task().then(() => {
+      expect(shellExecSpy).to.have.been.calledTwice;
+      expect(shellExecSpy.getCall(0).args[0]).to.equal(output('a', someFileContent));
+      expect(shellExecSpy.getCall(1).args[0]).to.equal(output('b', someOtherFileContent));
+    });
+  });
+
+  it('should not report a non existing bundle', () => {
+    const entry = {
+      a: 'src/a.js',
+      b: 'src/b.js'
+    };
+    test.setup({
+      'dist/b.bundle.min.js': someFileContent,
+      'fedops.json': fedopsJson
+    });
+    const task = createTask({projectConfig: {entry: () => entry}});
+    return task().then(() => {
+      expect(shellExecSpy).to.have.been.calledOnce;
+      expect(shellExecSpy).to.have.been.calledWith(output('b', someFileContent));
     });
   });
 });
