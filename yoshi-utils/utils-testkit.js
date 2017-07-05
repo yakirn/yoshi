0f492adca6e2a7a98023d47603043d9005e26ebc
Yaniv Efraim <yaniv.efraim@gmail.com>
2017-07-05 15:36:42 +0300

    add yoshi-utils testkit

---
+++ yoshi-utils/utils-testkit.js
@@ -0,0 +1,12 @@
+let oldEnv = process.env;
+module.exports.cleanMocks = () => {
+  process.env = oldEnv;
+};
+
+module.exports.mockProduction = (isProduction = true) => {
+  if (isProduction) {
+    process.env.NODE_ENV = 'production';
+  } else {
+    delete process.env.NODE_ENV;
+  }
+}
