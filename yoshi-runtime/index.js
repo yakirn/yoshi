ba2a06e453752b745303d4c46992738d2555cd08
Yaniv Efraim <yaniv.efraim@gmail.com>
2017-07-09 18:17:25 +0300

    add first stage of css modules (wip)

--- yoshi-runtime/index.js
+++ yoshi-runtime/index.js
@@ -1,3 +1,16 @@
 const {isProduction, isCI} = require('yoshi-utils');
+const genericNames = require('generic-names');
 
-module.exports.cssModulesPattren = () => (isProduction() || isCI()) ? `[hash:base64:5]` : `[path][name]__[local]__[hash:base64:5]`;
+const cssModulesPattren = module.exports.cssModulesPattren = () => (isProduction() || isCI()) ? `[hash:base64:5]` : `[path][name]__[local]__[hash:base64:5]`;
+
+module.exports.configCssModules = rootDir => {
+  require('css-modules-require-hook')({
+    rootDir,
+    generateScopedName: (name, filepath) => {
+      let generate = genericNames(cssModulesPattren(), {context: rootDir});
+      return generate(name, filepath);
+    },
+    extensions: ['.scss', '.css'],
+    camelCase: true
+  });
+};
--- yoshi-runtime/test/runtime.spec.js
+++ yoshi-runtime/test/runtime.spec.js
@@ -1,6 +1,7 @@
 const {expect} = require('chai');
 const {cssModulesPattren} = require('../index');
 const {cleanMocks, mockEnvironment, mockCI} = require('yoshi-utils').utilsTestkit;
+const {create} = require('test-phases');
 
 describe('CSS modules pattern', () => {
   afterEach(() => {
@@ -23,5 +24,38 @@ describe('CSS modules pattern', () => {
     mockEnvironment({production: false});
     mockCI({ci: true});
     expect(cssModulesPattren()).to.equal(`[hash:base64:5]`);
+  });
+});
+
+describe('CSS modules runtime', () => {
+  const generateCssModulesPattern = (name, path, pattern = `[hash:base64:5]`) => {
+    const genericNames = require('generic-names');
+    const generate = genericNames(pattern);
+    return generate(name, path);
+  };
+
+  afterEach(() => {
+    cleanMocks();
+  });
+
+  it('should generate css modules', () => {
+    mockEnvironment({production: true});
+    const hash = generateCssModulesPattern('a', 'styles/my-file.css');
+    const expectedCssMap = `{ a: '${hash}' }\n`;
+    const myTest = create('src/index');
+    const res = myTest
+      .setup({
+        'src/index.js': `const {configCssModules} = require('${require.resolve('../index')}');
+          configCssModules('./src');
+          const s = require('./styles/my-file.css')
+          console.log(s);
+        `,
+        'src/styles/my-file.css': `.a {color: red;}`
+      })
+      .execute('');
+
+    expect(res.code).to.equal(0);
+    expect(res.stdout).to.equal(expectedCssMap);
+    myTest.teardown();
   });
 });
